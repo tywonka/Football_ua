d36f29f8b81d2246cb52d326a5869a69643e95ce
Alex Lysenko <tywonka@gmail.com>
2012-03-07 21:18:52 +0200

    Add Google Chrome extension for football.ua site

---
+++ ChromeExtension/content_script.js
@@ -0,0 +1,14 @@
+﻿document.getElementsByClassName = function(cl) {
+	var retnode = [];
+	var myclass = new RegExp('.*'+cl);
+	var elem = this.getElementsByTagName('*');
+	for (var i = 0; i < elem.length; i++) {
+		var classes = elem[i].className;
+		if (myclass.test(classes)) retnode.push(elem[i]);
+	}
+	return retnode;
+};
+var targets = document.getElementsByClassName(' autblock');
+for (var i = 0; i < targets.length; ++i) {
+	targets[i].onclick = '';
+}
